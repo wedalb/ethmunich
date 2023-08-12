d322cbe20930cda48fc5f79349e629d4609c4371
alouiii <achref.aloui73@gmail.com>
2023-08-12 22:36:20 +0200

    - Adds form service

--- ionic-react-app/src/pages/AddTab.tsx
+++ ionic-react-app/src/pages/AddTab.tsx
@@ -1,8 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './AddTab.css';
 
+// const AddTab: React.FC = () => {
+//   return (
+//     <IonPage>
+//       <IonHeader>
+//         <IonToolbar>
+//           <IonTitle>Add Service</IonTitle>
+//         </IonToolbar>
+//       </IonHeader>
+//       <IonContent fullscreen>
+//         <IonHeader collapse="condense">
+//           <IonToolbar>
+//             <IonTitle size="large">Tab 2</IonTitle>
+//           </IonToolbar>
+//         </IonHeader>
+//       </IonContent>
+//     </IonPage>
+//   );
+// };
+
+
+import React, { useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonTextarea,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonToast,
+} from '@ionic/react';
+
 const AddTab: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [showToast, setShowToast] = useState(false);
+
+  const handleSubmit = () => {
+    // You can handle the form submission logic here
+    console.log('Title:', title);
+    console.log('Description:', description);
+    console.log('Price:', price);
+    // Show the toast
+    setShowToast(true);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +58,40 @@ const AddTab: React.FC = () => {
           <IonTitle>Add Service</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonLabel position="floating">Title</IonLabel>
+          <IonInput
+            value={title}
+            onIonChange={(e) => setTitle(e.detail.value!)}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Description</IonLabel>
+          <IonTextarea
+            value={description}
+            onIonChange={(e) => setDescription(e.detail.value!)}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Price</IonLabel>
+          <IonInput
+            type="number"
+            value={price}
+            onIonChange={(e) => setPrice(e.detail.value!)}
+          />
+        </IonItem>
+        <IonButton expand="full" onClick={handleSubmit}>
+          Submit
+        </IonButton>
       </IonContent>
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message="Submission successful"
+        position='top'
+        duration={2000} // Duration in milliseconds
+      />
     </IonPage>
   );
 };
