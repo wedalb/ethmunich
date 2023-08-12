73a7fd248a2bc1f0316c62480f6ff214a05dd8c1
alouiii <achref.aloui73@gmail.com>
2023-08-12 16:53:33 +0200

    - web3 added

--- ionic-react-app/src/App.tsx
+++ ionic-react-app/src/App.tsx
@@ -33,10 +33,230 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { useEffect, useState } from 'react';
+import Web3, { Contract } from 'web3';
+import ETHService from "../abi/ETHServices.json"
+import { MetaMaskInpageProvider } from "@metamask/providers";
+
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
+}
+interface Service {
+  title: string;
+  description : string;
+  owner: string;
+  contestant: string;
+  amount: bigint;
+}
 
 setupIonicReact();
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+
+
+  // General state vars
+  const [web3, setWeb3] = useState<Web3>();
+  const [blockNumber, setBlockNumber] = useState<bigint>();
+
+  // Contract vars
+  const [ethService, setEthService] = useState<Contract<ETHService.abi>>();
+
+  // User related state vars
+  const [services, setServices] = useState<Service[]>();
+  const [account, setAccount] = useState<string>();
+  const [activeServiceOwners, setActiveServiceOwners] = useState<string[]>();
+
+
+  const [ethBalance, setEthBalance] = useState<string>();
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  /*   Check MetaMask exists and user account connected
+       Initialize state variables
+  */
+  const checkWalletConnection = async () => {
+    if (window.ethereum !== undefined) {
+      const web3 = new Web3(window.ethereum);
+      const netId = await web3.eth.net.getId();
+      // Accounts available on MetaMask should be accesible
+      // if MetaMask is connected
+      const accounts = await web3.eth.getAccounts();
+      // If account state var not initialized and MetMask connected
+      if (!account && accounts[0]) {
+        // Initialize web3
+        setWeb3(web3);
+        setBlockNumber(await web3.eth.getBlockNumber());
+
+        // Initalize contracts
+        const ethServiceContract = new web3.eth.Contract(
+          ETHService.abi, // Contract ABI
+          ETHService.networks[netId].address // Contract address
+        );
+        setEthService(ethServiceContract);
+
+        // Initialize user account
+        const balance = await web3.eth.getBalance(accounts[0]);
+        setAccount(accounts[0]);
+
+        setEthBalance(web3.utils.fromWei(balance, "ether"));
+        const activeOwners = await ethServiceContract?.methods.getDepositedAddresses().call();
+        console.log(activeOwners);
+        setActiveServiceOwners(activeOwners);
+        
+        const serviceArray : Service[] = [];
+        for (var activeOwner of activeOwners){
+          serviceArray.push(await ethServiceContract?.methods.services(activeOwner).call());
+        }
+
+        console.log(serviceArray);
+        setServices(serviceArray);
+
+
+        setIsWalletConnected(true);
+      }
+    } else {
+      alert("Make sure you have MetaMask installed!");
+      return;
+    }
+  };
+
+  const handleAccept = async () => {
+    try {
+      await ethService?.methods.acceptService().send({
+        from: account,
+      });
+      const activeOwners = await ethService?.methods.getDepositedAddresses().call();
+      console.log(activeOwners);
+      setActiveServiceOwners(activeOwners);
+      const serviceArray : Service[] = [];
+      for (var activeOwner of activeOwners){
+        serviceArray.push(await ethService?.methods.services(activeOwner).call());
+      }
+      console.log(serviceArray);
+      setServices(serviceArray);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
+  useEffect(() => {
+    checkWalletConnection();
+  }, []);
+
+  // Make deposit from user account
+  const handleDeposit = async () => {
+    try {
+      await bbseBank?.methods.deposit().send({
+        from: account,
+        value: web3.utils.toWei(newDeposit, "ether"),
+      });
+      setDeposit(await bbseBank.methods.investors(account).call());
+      setBlockNumber(await web3.eth.getBlockNumber());
+      const balance = await web3.eth.getBalance(account);
+      const tokenBalance = await bbseToken.methods.balanceOf(account).call();
+      setEthBalance(web3.utils.fromWei(balance, "ether"));
+      setBbseBalance(web3.utils.fromWei(tokenBalance, "ether"));
+      setNewDeposit(0);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Wihtdraw existing deposit
+  const handleWithdraw = async () => {
+    try {
+      await bbseBank?.methods.withdraw().send({
+        from: account,
+      });
+      setDeposit(await bbseBank.methods.investors(account).call());
+      setBlockNumber(await web3.eth.getBlockNumber());
+      const balance = await web3.eth.getBalance(account);
+      const tokenBalance = await bbseToken.methods.balanceOf(account).call();
+      setEthBalance(web3.utils.fromWei(balance, "ether"));
+      setBbseBalance(web3.utils.fromWei(tokenBalance, "ether"));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Take loan from BBSE Bank
+  const handleLoan = async () => {
+    try {
+      await bbseBank?.methods
+        .borrow(web3.utils.toWei(newLoan.toString(), "ether"))
+        .send({
+          from: account,
+        });
+      setLoan(await bbseBank.methods.borrowers(account).call());
+      setBlockNumber(await web3.eth.getBlockNumber());
+      const balance = await web3.eth.getBalance(account);
+      const tokenBalance = await bbseToken.methods.balanceOf(account).call();
+      setEthBalance(web3.utils.fromWei(balance, "ether"));
+      setBbseBalance(web3.utils.fromWei(tokenBalance, "ether"));
+      // Update allowance state var after loan is taken
+      const allowance = await bbseToken.methods
+        .allowance(account, bbseBank._address)
+        .call();
+      setAllowance(allowance);
+      setNewLoan(0);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Pay back existing loan
+  const handlePayLoan = async () => {
+    try {
+      await bbseBank?.methods.payLoan().send({
+        value: loan.amount,
+        from: account,
+      });
+      setLoan(await bbseBank.methods.borrowers(account).call());
+      setBlockNumber(await web3.eth.getBlockNumber());
+      const balance = await web3.eth.getBalance(account);
+      const tokenBalance = await bbseToken.methods.balanceOf(account).call();
+      setEthBalance(web3.utils.fromWei(balance, "ether"));
+      setBbseBalance(web3.utils.fromWei(tokenBalance, "ether"));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  // Set allowance from user to BBSE Bank
+  const handleApprove = async () => {
+    await bbseToken.methods
+      .approve(
+        bbseBank._address,
+        web3.utils.toWei(newAllowance.toString(), "ether")
+      )
+      .send({
+        from: account,
+      });
+    const allowance = await bbseToken.methods
+      .allowance(account, bbseBank._address)
+      .call();
+    setAllowance(allowance);
+    setNewAllowance(0);
+    setBlockNumber(await web3.eth.getBlockNumber());
+  };
+
+  // Handler to connect user MetaMask to app
+  const connectWalletHandler = async () => {
+    if (window.ethereum !== undefined) {
+      try {
+        // Request access to MetaMask accounts
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        checkWalletConnection();
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("Make sure you have MetaMask installed!");
+    }
+  };
+  return (
     <IonApp>
       <IonReactRouter>
         <IonTabs>
@@ -72,5 +292,7 @@ const App: React.FC = () => (
       </IonReactRouter>
     </IonApp>
 );
+
+}
 
 export default App;
