542166c8b28cb8437e947c210bb11651a5985706
Heidi Albarazi <heidialbarazi@MacBook-Pro-von-Heidi.local>
2023-08-12 09:46:26 +0200

    create project

---
+++ ethmunich/ethmunich/ethmunichApp.swift
@@ -0,0 +1,17 @@
+//
+//  ethmunichApp.swift
+//  ethmunich
+//
+//  Created by Heidi Albarazi on 12.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ethmunichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
