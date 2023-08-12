e38f16f095a5560fce4f2936afbda64c1b5c78d4
alimokni <ge36hok@mytum.de>
2023-08-12 16:02:25 +0200

    Woopti

---
+++ ionic-react-app/src/components/CardComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonLabel, IonGrid, IonRow, IonCol } from '@ionic/react';
+
+
+interface CardComponent {
+    title: string;
+    subtitle: string;
+    description: string;
+    number : number;
+}
+//{props.description}
+const CardComponent :  React.FC<CardComponent> = (props) => {
+    return (
+        <IonCard className="ion-card-with-shadow" >
+            <IonCardHeader>
+                <IonCardTitle> {props.title }</IonCardTitle>
+                <IonCardSubtitle> { props.subtitle }</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+                <IonLabel className="large-number card-header">{number}</IonLabel>
+            </IonCardContent>
+        </IonCard>
+    );
+}
+export default CardComponent;
--- ionic-react-app/src/pages/ServicesTab.tsx
+++ ionic-react-app/src/pages/ServicesTab.tsx
@@ -1,8 +1,19 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import CardComponent from '../components/CardComponent';
 import './ServicesTab.css';
 
+
+const items = {
+
+}
 const ServicesTab: React.FC = () => {
+    const items = [
+        { title: 'Fix Bench in your local park', subtitle: 'Someone vandalized the bench here.' },
+        { title: 'Repair Piping in the Animal Shelter', subtitle: 'The kitties are drippin\'  '},
+        { title: 'Help a grandma commit tax fraud', subtitle: 'Grandma Stevens is too old to pay taxes' },
+    ];
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +28,9 @@ const ServicesTab: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Services" />
+          {items.map((item, index) => (
+              <CardComponent key={index} title={item.title} subtitle={item.subtitle}  description={"Test"}/>
+          ))}
       </IonContent>
     </IonPage>
   );
